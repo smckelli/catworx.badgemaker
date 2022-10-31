9feb181d7452c12d2fa0148a5098a7490e39f083
smckelli <smckelli@gmail.com>
2022-10-31 13:05:44 -0500

    broken code

--- Program.cs
+++ Program.cs
@@ -4,20 +4,25 @@ using System.Collections.Generic;
 namespace CatWorx.BadgeMaker
 {
   class Program
-  {
-    static void Main(string[] args)
     {
-      List<string> employees = new List<string>() { "adam", "amy" };
-      employees.Add("barbara");
-      employees.Add("billy");
-      Console.WriteLine("Please enter a name: ");
-      // Get a name from the console and assign it to a variable
-      string input = Console.ReadLine() ?? "";
-      employees.Add(input);
-      for (int i = 0; i < employees.Count; i++) 
-      {
-        Console.WriteLine(employees[i]);
-      }
+        static void Main(string[] args)
+            {
+            List<string> employees = new List<string>();
+                // Collect user values until the value is an empty string
+                while (true)
+                {
+                    Console.WriteLine("Please enter a name: (leave empty to exit): ");
+                    string input = Console.ReadLine() ?? "";
+                    // Break if the user hits ENTER without typing a name
+                    if (input == "")
+                    {
+                        break;}
+                    }
+                    employees.Add(input);
+                } 
+                for (int i = 0; i < employees.Count; i++) 
+                {
+                    Console.WriteLine(employees[i]);
+                }
     }
-  }
 }
