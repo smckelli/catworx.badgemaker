f7165a30c2a023db887cf25658b19a3dc363c552
smckelli <smckelli@gmail.com>
2022-10-31 12:51:21 -0500

    starting c-sgarp tutorial

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CatWorx.BadgeMaker
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      List<string> employees = new List<string>() { "adam", "amy" };
+      employees.Add("barbara");
+      employees.Add("billy");
+      Console.WriteLine("Please enter a name: ");
+      // Get a name from the console and assign it to a variable
+      string input = Console.ReadLine() ?? "";
+      employees.Add(input);
+      for (int i = 0; i < employees.Count; i++) 
+      {
+        Console.WriteLine(employees[i]);
+      }
+    }
+  }
+}
